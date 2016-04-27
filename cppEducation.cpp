6aea54633a4249ae5be481e33f8e5be71d6fa1cf
chantebryana <chantebryana@gmail.com>
2016-04-27 10:55:13 -0500

    First Commit
    following tutorial on http://www.cplusplus.com/doc/tutorial

---
+++ cppEducation.cpp
@@ -0,0 +1,15 @@
+//my third program in C++: I figured it out wihtout being explicitly told!
+#include <iostream>
+
+int main ()
+{
+	int a = 5;
+	int b = 2;
+	a = a + 1;
+	int result = a - b;
+	std::cout << a;
+	std::cout << result;
+	//std::cout << "Hello World! ";
+	//std::cout << "I'm a C++ program";
+	//std::cout << " with more words!!";
+}
