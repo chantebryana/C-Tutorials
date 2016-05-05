b299dc4ada16b64e4ee3255e0ab1f4a41d180229
chantebryana <chantebryana@gmail.com>
2016-05-05 16:33:00 -0500

    Playing with floats, doubles, etc w/ Jim
    I tried multiplying large number (99.99 * 6999) and got rounding errors.
    Jim's explorations and comments helped to tease out why that may have
    happened.

--- cppEducation.cpp
+++ cppEducation.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
 using namespace std;
 
 int main ()
@@ -11,12 +12,18 @@ int main ()
 	float price = 0;
 	int quantity = 0;
 	
-	cout << "Enter price: ";
+	float t_s = 99.99f;
+	double t_l = 99.99;
+	printf("%.50f\n", t_s); //99.98999786376953125000000000000000000000000000000000
+	printf("%.50f\n", t_l); //99.98999999999999488409230252727866172790527000000000
+	
+	cout << "Enter price: "; //99.99 
 	getline (cin, mystr);
 	stringstream(mystr) >> price;
-	cout << "Enter quantity ";
+	cout << "Enter quantity "; //6999
 	getline (cin, mystr);
 	stringstream(mystr) >> quantity;
-	cout << "Total price: " << price*quantity << endl;
+	cout << "Total price: ";// << price*quantity << endl;
+	printf("%.2f\n", ((double)price)*quantity); //I kept getting rounding errors that seemed mysterious.  Lines 17 & 18 help to show how the binary fucks up my floats.
 	return 0;
 }
