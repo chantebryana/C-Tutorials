c79f6452d42927537a079fd572a1e5dd21aa9e8e
chantebryana <chantebryana@gmail.com>
2016-05-05 11:26:57 -0500

    more i/o wanderings

--- cppEducation.cpp
+++ cppEducation.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main ()
 {
 	int i;
-	cout << "Please enter an integer value: ";
-	cin >> i;
-	cout << "The value you entered is " << i;
-	cout << " and its double is " << i*2 << ".\n";
+	int j;
+	cout << "Please enter two integer values: ";
+	cin >> i >> j;
+	cout << "The value you entered is " << i << " and " << j;
+	//cout << " and its double is " << i*2 << ".\n";
 	return 0;
 }
