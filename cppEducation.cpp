381ab44c46d4e88a194502a8459627eaaf5cf125
chantebryana <chantebryana@gmail.com>
2016-05-05 11:24:39 -0500

    Playing with Input/Output
    http://www.cplusplus.com/doc/tutorial/basic_io/

--- cppEducation.cpp
+++ cppEducation.cpp
@@ -1,21 +1,14 @@
-//playing with strings and compound types (before I was using fundamental types)
-/*this time () didn't work for defining mystring.  Here's what my console told me: 
-Chauncy@DESKTOP-L5V77C9 ~/home/projects/C++ Tutorials
-$ g++ cppEducation.cpp -o cppEducation
-cppEducation.cpp: In function ‘int main()’:
-cppEducation.cpp:11:48: error: no match for call to ‘(std::string {aka std::basic_string<char>}) (const char [35])’
-  mystring ("This is a different string content");
-*/
+//exploring i/o concepts in http://www.cplusplus.com/doc/tutorial/basic_io/ 
+
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main ()
 {
-	string mystring;
-	mystring = "This is the initial string content";
-	cout << mystring << endl;
-	mystring = "This is a different string content";
-	cout << mystring << endl;
+	int i;
+	cout << "Please enter an integer value: ";
+	cin >> i;
+	cout << "The value you entered is " << i;
+	cout << " and its double is " << i*2 << ".\n";
 	return 0;
 }
